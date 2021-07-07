d0f2bc8d5b29aa40e56e11b038ab1189cb9a8332
Denis Dorogov <denisdorogov@yandex.ru>
2021-07-07 08:26:15 +0500

    created the seeder for DB users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'denis',
+            'email' => 'denis@gmail.com',
+            'password' => Hash::make('123456'),
+        ]);
+        User::factory(10)->create();
     }
 }
